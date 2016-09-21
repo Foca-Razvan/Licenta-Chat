45e55fc04712499cc7e25e7e069cae9faaffeb3a
Foca Razvan-Andrei <razvan_andrei94@yahoo.com>
2016-09-21 18:20:32 +0300

    commit

--- Server/Program.cs
+++ Server/Program.cs
@@ -14,13 +14,13 @@ namespace Server
         static void Main(string[] args)
         {
 
-            ServiceHost serviceHost = new ServiceHost(typeof(ConnectionService), new Uri("net.tcp://localhost:4444/ConnectionService"));
+            ServiceHost serviceHost = new ServiceHost(typeof(ConnectionService), new Uri("net.tcp://192.168.0.100:4444/ConnectionService"));
             serviceHost.AddServiceEndpoint(typeof(IConnection), new NetTcpBinding(), "");
 
-            ServiceHost serviceCommunicationHost = new ServiceHost(typeof(CommunicationService), new Uri("net.tcp://localhost:4444/CommunicationService"));
+            ServiceHost serviceCommunicationHost = new ServiceHost(typeof(CommunicationService), new Uri("net.tcp://192.168.0.100:4444/CommunicationService"));
             serviceCommunicationHost.AddServiceEndpoint(typeof(ICommunication), new NetTcpBinding(), "");
 
-            ServiceHost serviceAudioHost = new ServiceHost(typeof(AudioService), new Uri("net.tcp://localhost:4444/AudioService"));
+            ServiceHost serviceAudioHost = new ServiceHost(typeof(AudioService), new Uri("net.tcp://192.168.0.100:4444/AudioService"));
             serviceAudioHost.AddServiceEndpoint(typeof(IAudio), new NetTcpBinding(), "");
 
             serviceCommunicationHost.Open();
